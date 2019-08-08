6936eb6b2ffbc0dc617d2ee72552f160ab746186
derekalangilbert <derek@thedevelopmentphase.com>
2019-08-07 19:56:02 -0600

    created a casino slot

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'always',
+	htmlWhitespaceSensitivity: 'strict',
+	proseWrap: 'never',
+	endOfLine: 'lf'
+}
